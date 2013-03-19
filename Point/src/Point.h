70b067656ab4b265e3ac16001e6d24564f9e2d83
Jonny Frey <j0nn9.fr39@gmail.com>
2013-03-19 23:24:47 +0100

    mprooved readability in point

--- Point/src/Point.h
+++ Point/src/Point.h
@@ -1,38 +1,42 @@
 /**
- * This Structur holds the a coordinate in a
- * two dimensional field
+ * Header of an simple two dimensional point impelementation
+ * as a struct and a uint64_t which uses the higher 32 bits
+ * as y and the lower as x coordianate
  */
-#ifndef POINT
-#define POINT
+#ifndef __POINT_H__
+#define __POINT_H__
 #include <sys/types.h>
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-  u_int32_t x, y;
+  uint32_t x, y;
 } Point;
 
 /**
- * Converts an given Point to an u_int64_t
+ * Converts an given Point to an uint64_t
  */
-#define POINT_TO_UINT(POINT) ((((u_int64_t) POINT.y) << 32) + ((u_int64_t) POINT.x))
+#define POINT_TO_UINT(POINT)                                    \
+  ((((uint64_t) (POINT).y) << 32) + ((uint64_t) (POINT).x))
 
 /**
- * Converts an given u_int64_t to an Point
+ * Converts an given uint64_t to an Point
  */
-#define UINT_TO_POINT(UINT, POINT)      \
-  POINT.x = (u_int32_t) UINT;           \
-  POINT.y = (u_int32_t) (UINT >> 32);
-
-#endif // end of Point.h
+#define UINT_TO_POINT(UINT, POINT)              \
+  do {                                          \
+    (POINT).x = (uint32_t) (UINT);              \
+    (POINT).y = (uint32_t) ((UINT) >> 32);      \
+  } while (0)
 
 /**
- * Returns wether th given Points are equal
+ * Returns wether the given Points are equal
  */
-#define POINTS_EQUAL(A, B) ((A.x == B.x) && (A.y == B.y))
+#define POINTS_EQUAL(A, B) (((A).x == (B).x) && ((A).y == (B).y))
 
 /**
  * Returns wether the given Points are not equal
  */
-#define POINTS_NOT_EQUAL(A, B) ((A.x != B.x) || (A.y != B.y))
+#define POINTS_NOT_EQUAL(A, B) (((A).x != (B).x) || ((A).y != (B).y))
+
+#endif // __POINT_H__
