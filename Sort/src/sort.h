bcf2b6b374b498f42b08810e918a5c91ff02c69e
Jonny Frey <j0nn9.fr39@gmail.com>
2013-03-20 14:57:18 +0100

    added macros for insertionsort and quich_insert_sort (sort)

--- Sort/src/sort.h
+++ Sort/src/sort.h
@@ -4,31 +4,114 @@
  */
 #include <math.h>
 #include <pthread.h>
-#include "Array.h"
+#include <inttypes.h>
+#include "../../Dynamic-Array/src/dynamic-array.h"
 
 /**
- * The Element Type of the Array to sort
+ * Stack for non recursive Quicksort
  */
-#ifndef CHIEFSORT_TYPE
-#define CHIEFSORT_TYPE int
-#endif
+DEFINE_ARRAY(uint64_t, SortUInt64Ary)
 
-#ifndef CHIEFSORT_BIGGER
-#define CHIEFSORT_BIGGER(X, Y) X > Y
-#endif
+/**
+ * Just Insertionsort
+ *
+ * this macro needs the TYPE of the Array Elements
+ * and an smaler function which compares two array elements
+ */
+#define INSERTIONSORT(TYPE, ARY, LEN, SMALER)                   \
+  do {                                                          \
+    int is_i, is_j;                                             \
+    TYPE is_temp;                                               \
+                                                                \
+    for (is_i = 1; is_i < LEN; is_i++) {                        \
+      is_temp = ARY[is_i];                                      \
+      is_j = is_i - 1;                                          \
+                                                                \
+      while (SMALER(is_temp, ARY[is_j]) && is_j >= 0) {         \
+        ARY[is_j + 1] = ARY[is_j];                              \
+        is_j = is_j - 1;                                        \
+      }                                                         \
+                                                                \
+      ARY[is_j + 1] = is_temp;                                  \
+    }                                                           \
+  } while (0)                                                   
 
-#ifndef CHIEFSORT_SMALER
-#define CHIEFSORT_SMALER(X, Y) X < Y
-#endif
 
-#ifndef CHIEFSORT_EQL
-#define CHIEFSORT_EQL(X, Y) X == Y
-#endif
+/**
+ * Improofed variant of QuickSort which uses insertionsort at 
+ * a specific min array size
+ *
+ * This Macro needs the ARY element TYPE also a 
+ * BIGGER, SMALER and EQL function which compares two ARY elements
+ * MIN is the minimal array length to progess Quicksort 
+ *     after MIN insertionsort will be used
+ */
+#define QUICK_INSERT_SORT(TYPE, ARY, LEN, BIGGER, SMALER, EQL, MIN)           \
+  do {                                                                        \
+                                                                              \
+    TYPE qis_piv;                                                             \
+    TYPE qis_temp;                                                            \
+    SortUInt64Ary qis_stack;                                                  \
+    uint64_t qis_l, qis_r, qis_left, qis_right;                               \
+    ARY_INIT(uint64_t,                                                        \
+             qis_stack,                                                       \
+             (uint64_t) 5 * (log2((double) (LEN)) / 3));                      \
+                                                                              \
+    qis_left  = 0;                                                            \
+    qis_right = (LEN) - 1;                                                    \
+                                                                              \
+    while (1) {                                                               \
+                                                                              \
+      if (qis_right - qis_left < MIN) {                                       \
+        INSERTIONSORT(TYPE,                                                   \
+                      ARY + qis_left,                                         \
+                      (qis_right - qis_left) + 1,                             \
+                      SMALER);                                                \
+                                                                              \
+        qis_left = qis_right;                                                 \
+      }                                                                       \
+                                                                              \
+      if (qis_left < qis_right) {                                             \
+                                                                              \
+        qis_l = qis_left;                                                     \
+        qis_r = qis_right;                                                    \
+        qis_piv = ARY[(rand() % (qis_r - qis_l)) + qis_l];                    \
+                                                                              \
+        while (qis_l < qis_r) {                                               \
+                                                                              \
+          while (BIGGER(ARY[qis_r], qis_piv))                                 \
+            qis_r--;                                                          \
+                                                                              \
+          while (SMALER(ARY[qis_l], qis_piv))                                 \
+            qis_l++;                                                          \
+                                                                              \
+          if (EQL(ARY[l], ARY[r])) {                                          \
+            qis_r--;                                                          \
+                                                                              \
+          /* switch left an right */                                          \
+          } else if (qis_l < qis_r) {                                         \
+            qis_temp = ARY[qis_l];                                            \
+            ARY[qis_l] = ARY[qis_r];                                          \
+            ARY[qis_r] = qis_temp;                                            \
+          }                                                                   \
+                                                                              \
+        }                                                                     \
+                                                                              \
+        ARY_PUSH(uint64_t, qis_stack, qis_right);                             \
+        qis_right = (qis_left > qis_l - 1) ? qis_left : qis_l - 1;            \
+                                                                              \
+      } else {                                                                \
+        if (qis_stack.length > 0) {                                           \
+          qis_left = qis_right + 1;                                           \
+          ARY_PULL(qis_stack, qis_right);                                     \
+        } else break;                                                         \
+      }                                                                       \
+                                                                              \
+    }                                                                         \
+                                                                              \
+  } while (0)                                                                 
+                                                                              
 
-#ifndef U_INT_ARY
-#define U_INT_ARY
-DEFINE_ARRAY(int, IntAry)
-#endif
 
 /**
  * Struct for handling paralell sorting
