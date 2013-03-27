75ce53ab629128e5f694197bdd8cfdfe36ef7c45
Jonny Frey <j0nn9.fr39@gmail.com>
2013-03-27 16:27:20 +0100

    added addition and division (time diff)

--- Time-Diff/src/time-diff.c
+++ Time-Diff/src/time-diff.c
@@ -21,6 +21,39 @@ int timeval_subtract(struct timeval *result,
 }
 
 /**
+ * Return 1 if the addition is negative, otherwise 0.  
+ * Saves the Addition of t1 and t2 in result
+ */
+int timeval_additon(struct timeval *result, 
+                    struct timeval *t2, 
+                    struct timeval *t1) {
+
+    long int diff =   (t2->tv_usec + 1000000 * t2->tv_sec) 
+                    + (t1->tv_usec + 1000000 * t1->tv_sec);
+
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+
+    return (diff < 0);
+}
+
+/**
+ * Returns 1 if the Division is negative, otherwise 0.
+ * Saves the Division of t1 by divisor in result
+ */
+int timeval_additon(struct timeval *result, 
+                    int divisor, 
+                    struct timeval *t1) {
+
+    long int diff =   (t2->tv_usec + 1000000 * t2->tv_sec) / divisor
+
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+
+    return (diff < 0);
+}
+
+/**
  * Prints a Timevall struct
  */
 void timeval_print(struct timeval *tv) {
