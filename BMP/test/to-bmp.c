4c4aaab1c0af70259e788cc9f2d15b87b09efb25
Jonny Frey <j0nn9.fr39@gmail.com>
2013-04-17 17:21:35 +0200

    added to-bmp test

---
+++ BMP/test/to-bmp.c
@@ -0,0 +1,57 @@
+/**
+ * converts any fiel to RGB-BMP
+ */
+#include <inttypes.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include "../src/bmp.h"
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("%s <file>\n", argv[0]);
+    exit(1);
+  }
+
+  struct stat st;
+  stat(argv[1], &st);
+  uint32_t f_size = st.st_size;
+
+  int f = open(argv[1], O_RDONLY, 0);
+  char *f_data  = mmap(NULL, f_size, PROT_READ, MAP_SHARED, f, 0);
+  uint32_t bmp_size = (uint32_t) sqrt((double) f_size);
+
+  BMP bmp;
+  init_RGB_BMPHeader(&bmp.head, bmp_size / 3, bmp_size);
+
+  char buffer[256];
+  sprintf(buffer, "%s.bmp", argv[1]);
+  
+  int fd = open(buffer, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+  if(fd == -1) {
+    printf("failed to open %s\n", buffer);
+    exit(1);
+  }
+
+  if(bmp_write_header(&bmp.head, fd) == -1) {
+    printf("in main() while writing header to file\n");
+    exit(1);
+  }
+
+  if(write(fd, f_data, bmp_size * bmp_size) == -1) {
+    printf("in main() while writing body to file\n");
+    exit(1);
+  }
+   
+  return 0;
+}
+
