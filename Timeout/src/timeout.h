47f8a8db842d0d1de025fa848c69e9e774f46e28
Jonny Frey <j0nn9.fr39@gmail.com>
2013-10-18 15:47:48 +0200

    removed vfork timout, because the dangerous of messing stdlibs up when canceling from shared memory

--- Timeout/src/timeout.h
+++ Timeout/src/timeout.h
@@ -10,60 +10,6 @@
 #include <linux/sched.h>
 
 /**
- * global variable to determinate whether a function was 
- * shutdown by timout or not
- */
-static int __was_timouted__ = 0;
-
-/**
- * Process an function wih an agressive Timout
- * the given function will be runn in a new thread
- * sharing the same address space and file pointers.
- *
- * The timout is processed du to an SIGALARM
- * NOTE: this is my be very dangerous because it interuppts 
- * the called function within the same address space and so 
- * my some library static buffes have invalid values and behave unexepected
- */
-#define WITH_TIMEOUT_DO(USEC, FUNC, ...)                                      \
-do {                                                                          \
-                                                                              \
-  /* timer value */                                                           \
-  struct itimerval __timer_val__ = { { 0,  0 },                               \
-                                   { (USEC) / 1000000, (USEC) % 1000000 } };  \
-                                                                              \
-  pid_t __pid__ = vfork();                                                    \
-                                                                              \
-  if (__pid__ < 0) {                                                          \
-    perror("vfork failed in WITH_TIMEOUT_DO");                                \
-    exit(EXIT_FAILURE);                                                       \
-                                                                              \
-  } else if (__pid__ > 0) {                                                   \
-    waitpid(__pid__, &__was_timouted__, 0);                                   \
-                                                                              \
-  } else {                                                                    \
-                                                                              \
-    /* reset timer siganl handler */                                          \
-    signal(SIGALRM, SIG_DFL);                                                 \
-                                                                              \
-    /* start timer */                                                         \
-    setitimer(ITIMER_REAL, &__timer_val__, NULL);                             \
-                                                                              \
-    /* runn function */                                                       \
-    FUNC(__VA_ARGS__);                                                        \
-                                                                              \
-    /* use agresive exit to not destroy the parents stack */                  \
-    _exit(EXIT_SUCCESS);                                                      \
-  }                                                                           \
-} while (0)
-
-/**
- * returns wheter the last timout-function was timouted
- */
-#define TIMEOUT_REATCHED                                                    \
-  (WIFSIGNALED(__was_timouted__) && WTERMSIG(__was_timouted__) == SIGALRM)
-
-/**
  * Process an function wih an agressive Timout
  * the given funktion pointer will pe brocessed in
  * a seperate process so if it will be canceld 
