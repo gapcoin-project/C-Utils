8e3d9f43748ba31ad32d0ca86f33d09fadfd327d
Jonny Frey <j0nn9.fr39@gmail.com>
2013-10-18 15:46:59 +0200

    added new timout method with vfork

--- Timeout/src/timeout.h
+++ Timeout/src/timeout.h
@@ -4,24 +4,68 @@
 #ifndef __TIMEOUT_H__
 #define __TIMEOUT_H__
 
-#include <sys/shm.h>
-#include <sys/shm.h>
-#include <errno.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <inttypes.h>
-#include <sched.h>
+#include <sys/time.h>
 #include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include "../../Time-Diff/src/time-diff.h"
+#include <signal.h>
+#include <linux/sched.h>
+
+/**
+ * global variable to determinate whether a function was 
+ * shutdown by timout or not
+ */
+static int __was_timouted__ = 0;
 
 /**
  * Process an function wih an agressive Timout
- * th given funktion pointer will pe brocessed in
+ * the given function will be runn in a new thread
+ * sharing the same address space and file pointers.
+ *
+ * The timout is processed du to an SIGALARM
+ * NOTE: this is my be very dangerous because it interuppts 
+ * the called function within the same address space and so 
+ * my some library static buffes have invalid values and behave unexepected
+ */
+#define WITH_TIMEOUT_DO(USEC, FUNC, ...)                                      \
+do {                                                                          \
+                                                                              \
+  /* timer value */                                                           \
+  struct itimerval __timer_val__ = { { 0,  0 },                               \
+                                   { (USEC) / 1000000, (USEC) % 1000000 } };  \
+                                                                              \
+  pid_t __pid__ = vfork();                                                    \
+                                                                              \
+  if (__pid__ < 0) {                                                          \
+    perror("vfork failed in WITH_TIMEOUT_DO");                                \
+    exit(EXIT_FAILURE);                                                       \
+                                                                              \
+  } else if (__pid__ > 0) {                                                   \
+    waitpid(__pid__, &__was_timouted__, 0);                                   \
+                                                                              \
+  } else {                                                                    \
+                                                                              \
+    /* reset timer siganl handler */                                          \
+    signal(SIGALRM, SIG_DFL);                                                 \
+                                                                              \
+    /* start timer */                                                         \
+    setitimer(ITIMER_REAL, &__timer_val__, NULL);                             \
+                                                                              \
+    /* runn function */                                                       \
+    FUNC(__VA_ARGS__);                                                        \
+                                                                              \
+    /* use agresive exit to not destroy the parents stack */                  \
+    _exit(EXIT_SUCCESS);                                                      \
+  }                                                                           \
+} while (0)
+
+/**
+ * returns wheter the last timout-function was timouted
+ */
+#define TIMEOUT_REATCHED                                                    \
+  (WIFSIGNALED(__was_timouted__) && WTERMSIG(__was_timouted__) == SIGALRM)
+
+/**
+ * Process an function wih an agressive Timout
+ * the given funktion pointer will pe brocessed in
  * a seperate process so if it will be canceld 
  * by timout the kernle frees unfreed memory
  *
