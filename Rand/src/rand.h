e55767f778ee035085e51f1d90020dfbb4c63a0d
Jonny Frey <j0nn9.fr39@gmail.com>
2015-06-24 00:21:33 +0200

    made rand functions static

--- Rand/src/rand.h
+++ Rand/src/rand.h
@@ -3,7 +3,7 @@
 
 #include <inttypes.h>
 
-inline uint32_t rand32(uint32_t *x) {
+static inline uint32_t rand32(uint32_t *x) {
   *x ^= *x << 13;
   *x ^= *x >> 17;
   *x ^= *x << 5;
@@ -15,7 +15,7 @@ typedef struct {
   uint32_t x, y, z, w;
 } rand128_t; 
 
-inline rand128_t *new_rand128(uint32_t seed) {
+static inline rand128_t *new_rand128(uint32_t seed) {
   rand128_t *rand = (rand128_t *) malloc(sizeof(rand128_t));
 
   rand->x = rand32(&seed);
@@ -26,7 +26,7 @@ inline rand128_t *new_rand128(uint32_t seed) {
   return rand;
 }
                       
-inline uint32_t rand128(rand128_t *rand) {
+static inline uint32_t rand128(rand128_t *rand) {
                                
   uint32_t t = rand->x ^ (rand->x << 11);
   rand->x = rand->y; rand->y = rand->z; rand->z = rand->w;
